86e0bf9320058b9b7b9f18dbfb3a5d9275034b98
zondahuman <zondahuman@gmail.com>
2018-03-20 11:43:15 +0800

    add

--- src/main/java/com/abin/lee/sql/incubator/group/GroupSorting.java
+++ src/main/java/com/abin/lee/sql/incubator/group/GroupSorting.java
@@ -1,5 +1,12 @@
 package com.abin.lee.sql.incubator.group;
 
+import sun.util.calendar.BaseCalendar;
+import sun.util.calendar.Gregorian;
+import sun.util.calendar.JulianCalendar;
+import sun.util.calendar.LocalGregorianCalendar;
+
+import java.util.Timer;
+
 /**
  * Created by abin on 2018/3/19 18:36.
  * sql-incubator
@@ -7,6 +14,11 @@ package com.abin.lee.sql.incubator.group;
  */
 public class GroupSorting {
 
-
+    public static void main(String[] args) {
+        System.out.println(System.nanoTime());
+        System.out.println(System.currentTimeMillis());
+        System.out.println(LocalGregorianCalendar.Date.TIME_UNDEFINED);
+        System.out.println(Gregorian.getGregorianCalendar().getCalendarDate());
+    }
 
 }
